026e51b1333049b8622816c98f17834bb857a921
beka iremadze <beqa943@gmail.com>
2022-07-05 14:32:19 +0400

    ASD

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
-
-$(document).ready(function(){
-
-
-	/* ---- Countdown timer ---- */
-
-	$('#counter').countdown({
-		timestamp : (new Date()).getTime() + 08*10*60*60*1000
-	});
-
-
-	/* ---- Animations ---- */
-
-	$('#links a').hover(
-		function(){ $(this).animate({ left: 3 }, 'fast'); },
-		function(){ $(this).animate({ left: 0 }, 'fast'); }
-	);
-
-	$('footer a').hover(
-		function(){ $(this).animate({ top: 3 }, 'fast'); },
-		function(){ $(this).animate({ top: 0 }, 'fast'); }
-	);
-
-
-
-
-});
+function updateTimer() {
+	future = Date.parse("sep 27 , 2022 17:30:00");
+	now = new Date();
+	diff = future - now;
+   
+	days = Math.floor(diff / (1000 * 60 * 60 * 24));
+	hours = Math.floor(diff / (1000 * 60 * 60));
+	mins = Math.floor(diff / (1000 * 60));
+	secs = Math.floor(diff / 1000);
+   
+	d = days;
+	h = hours - days * 24;
+	m = mins - hours * 60;
+	s = secs - mins * 60;
+   
+	document.getElementById("timer").innerHTML =
+	  "<div>" +
+	  d +
+	  "<span>days</span></div>" +
+	  "<div>" +
+	  h +
+	  "<span>hours</span></div>" +
+	  "<div>" +
+	  m +
+	  "<span>minutes</span></div>" +
+	  "<div>" +
+	  s +
+	  "<span>seconds</span></div>";
+  }
+  setInterval("updateTimer()", 1000);
